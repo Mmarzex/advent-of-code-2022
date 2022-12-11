c8bb0a91c6740553f460a8fa4e432607cc5e8b37
Max Marze <max@marze.dev>
2022-12-11 11:37:14 -0500

    Cleanup Day 11

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,18 +1,35 @@
 use std::collections::VecDeque;
 
-
+#[derive(Clone, Debug)]
+enum Operation {
+    Addition,
+    Multiplication,
+}
 
 #[derive(Clone, Debug)]
-struct Monkey<'a> {
+enum OperationSide {
+    Number(usize),
+    Old,
+}
+
+#[derive(Clone, Debug)]
+struct Equation {
+    operation: Operation,
+    left: OperationSide,
+    right: OperationSide,
+}
+
+#[derive(Clone, Debug)]
+struct Monkey {
     checks: usize,
     items: VecDeque<usize>,
-    operation: &'a str,
+    equation: Equation,
     test: usize,
     test_true: usize,
     test_false: usize,
 }
 
-impl Monkey<'_> {
+impl Monkey {
     fn new(input: &str) -> Monkey {
         let lines = input.trim_end().lines().collect::<Vec<&str>>();
 
@@ -24,7 +41,32 @@ impl Monkey<'_> {
             .map(|i| i.parse::<usize>().unwrap())
             .collect::<VecDeque<usize>>();
 
-        let operation = lines[2].split(" = ").nth(1).unwrap();
+        let operation_parts = lines[2]
+            .split(" = ")
+            .nth(1)
+            .unwrap()
+            .split(' ')
+            .collect::<Vec<&str>>();
+        let op_left = if operation_parts[0] == "old" {
+            OperationSide::Old
+        } else {
+            OperationSide::Number(operation_parts[0].parse::<usize>().unwrap())
+        };
+        let op_right = if operation_parts[2] == "old" {
+            OperationSide::Old
+        } else {
+            OperationSide::Number(operation_parts[2].parse::<usize>().unwrap())
+        };
+
+        let equation = Equation {
+            left: op_left,
+            right: op_right,
+            operation: match operation_parts[1] {
+                "+" => Operation::Addition,
+                "*" => Operation::Multiplication,
+                &_ => panic!(),
+            },
+        };
 
         let test = lines[3]
             .split("by ")
@@ -49,71 +91,34 @@ impl Monkey<'_> {
 
         Monkey {
             checks: 0,
-            items: items,
-            operation: operation,
-            test: test,
-            test_true: test_true,
-            test_false: test_false,
+            items,
+            equation,
+            test,
+            test_true,
+            test_false,
         }
     }
 
-    fn process(&mut self) -> Option<(usize, usize)> {
+    fn process(&mut self, worry_reducer: impl Fn(usize) -> usize) -> Option<(usize, usize)> {
         if let Some(item) = self.items.pop_front() {
             self.checks += 1;
-            let operation_parts = self.operation.split(' ').collect::<Vec<&str>>();
-            let op_left = if operation_parts[0] == "old" {
-                item.clone()
-            } else {
-                operation_parts[0].parse::<usize>().unwrap()
+            let left = match self.equation.left {
+                OperationSide::Number(n) => n,
+                OperationSide::Old => item,
             };
-            // let op = operation_parts[1];
-            let op_right = if operation_parts[2] == "old" {
-                item.clone()
-            } else {
-                operation_parts[2].parse::<usize>().unwrap()
+            let right = match self.equation.right {
+                OperationSide::Number(n) => n,
+                OperationSide::Old => item,
             };
-
-            let mut new_value = match operation_parts[1] {
-                "+" => op_left + op_right,
-                "*" => op_left * op_right,
-                _ => panic!(),
+            let mut new_value = match self.equation.operation {
+                Operation::Addition => left + right,
+                Operation::Multiplication => left * right,
             };
 
-            new_value /= 3;
+            // new_value /= 3;
 
-            if new_value % self.test == 0 {
-                return Option::Some((new_value, self.test_true));
-            } else {
-                return Option::Some((new_value, self.test_false));
-            }
-        }
+            new_value = worry_reducer(new_value);
 
-        None
-    }
-
-    fn process_p2(&mut self, m: usize) -> Option<(usize, usize)> {
-        if let Some(item) = self.items.pop_front() {
-            self.checks += 1;
-            let operation_parts = self.operation.split(' ').collect::<Vec<&str>>();
-            let op_left = if operation_parts[0] == "old" {
-                item.clone()
-            } else {
-                operation_parts[0].parse::<usize>().unwrap()
-            };
-            // let op = operation_parts[1];
-            let op_right = if operation_parts[2] == "old" {
-                item.clone()
-            } else {
-                operation_parts[2].parse::<usize>().unwrap()
-            };
-
-            let mut new_value = match operation_parts[1] {
-                "+" => op_left + op_right,
-                "*" => op_left * op_right,
-                _ => panic!(),
-            };
-
-            new_value %= m;
             if new_value % self.test == 0 {
                 return Option::Some((new_value, self.test_true));
             } else {
@@ -135,16 +140,16 @@ pub fn part_one(input: &str) -> Option<usize> {
     for _ in 0..20 {
         for id in 0..monkeys.len() {
             while monkeys[id].items.len() > 0 {
-                if let Some((item, monkey_id)) = monkeys[id].process() {
+                if let Some((item, monkey_id)) = monkeys[id].process(|i| i / 3) {
                     monkeys[monkey_id].items.push_back(item);
                 }
             }
         }
     }
 
-    let mut monkey_business_values = monkeys.iter().map(|m| m.checks).collect::<Vec<usize>>();
-    monkey_business_values.sort();
-    let monkey_business: usize = monkey_business_values.iter().rev().take(2).product();
+    monkeys.sort_by(|a, b| a.checks.cmp(&b.checks));
+
+    let monkey_business: usize = monkeys.iter().rev().take(2).map(|m| m.checks).product();
     Option::Some(monkey_business)
 }
 
@@ -160,16 +165,16 @@ pub fn part_two(input: &str) -> Option<usize> {
     for _ in 0..10000 {
         for id in 0..monkeys.len() {
             while monkeys[id].items.len() > 0 {
-                if let Some((item, monkey_id)) = monkeys[id].process_p2(m) {
+                if let Some((item, monkey_id)) = monkeys[id].process(|i| i % m) {
                     monkeys[monkey_id].items.push_back(item);
                 }
             }
         }
     }
 
-    let mut monkey_business_values = monkeys.iter().map(|m| m.checks).collect::<Vec<usize>>();
-    monkey_business_values.sort();
-    let monkey_business: usize = monkey_business_values.iter().rev().take(2).product();
+    monkeys.sort_by(|a, b| a.checks.cmp(&b.checks));
+
+    let monkey_business: usize = monkeys.iter().rev().take(2).map(|m| m.checks).product();
     Option::Some(monkey_business)
 }
 
